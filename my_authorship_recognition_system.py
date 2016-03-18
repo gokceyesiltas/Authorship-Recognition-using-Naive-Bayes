e00655a971c250dd2912452b101ca6e6e0508431
gokceyesiltas <gokce@yesiltas.net>
2016-03-18 21:06:48 +0200

    Update my_authorship_recognition_system.py

--- my_authorship_recognition_system.py
+++ my_authorship_recognition_system.py
@@ -3,21 +3,27 @@ import os, sys, re
 from collections import Counter
 from math import log, log10
 
-fileEncoding = "ISO-8859-9"
-functionalWords = []
-vocabSize = 0
-featuresOfClasses = []
-vocabSizeEx = 0
-featuresOfClassesEx = []
-trainingDataSize = 0
-confusionMatrix = []
-classes = []
-truePositiveOfClass = []
-totalAssignedAsClass = Counter()
-totalTrueClass = []
-recalls = []
-precisions = []
+fileEncoding = "ISO-8859-9"		# for turkish character encoding
+functionalWords = []			# functional words 
+vocabSize = 0					# the number of unique words in whole training set for BOW training
+featuresOfClasses = []			# feature vectors of classes for BOW training
+vocabSizeEx = 0					# the number of unique words in whole training set for BOW + extra feature training
+featuresOfClassesEx = []		# feature vectors of classes for BOW + extra feature training
+trainingDataSize = 0			# number of documents in training set
+confusionMatrix = []			# confudion matrix 
+classes = []					# class names in training set
+truePositiveOfClass = []		# class name - true positive in test set
+totalAssignedAsClass = Counter() # class name - the number of assignments to the class in test set
+totalTrueClass = []				# class name - the true number of document in the class in test set
+recalls = []					# class name - recall values
+precisions = []					# class name - precision values
 
+
+'''
+	tokenizes the file according to white spaces.
+	deletes punctuation marks.
+	returns words in a list
+'''
 def tokenize(file):
 	lines = file.readlines()
 	article = " ".join(lines).strip()
@@ -32,6 +38,20 @@ def tokenize(file):
 			wordsWOP.append(wordWOP)
 	return wordsWOP
 
+'''
+	takes training path.
+	for all directories in training set, looks at all files in the directory. 
+	takes tokens by tokenize() function.
+	for these words creates a counter which counts the unique words in the list.
+	by adding counters, creates a total counter for each directory.
+	creates a quadruple for each directory.
+	that quadruple contains: class name
+							 the number of documents in class
+							 the number of words in class
+							 unique word - frequency pairs in class
+	also calculates the total number of files in whole training set and
+					the number of unique words in whole training set.
+'''
 def trainBOW(trainingPath):
 	global vocabSize
 	global featuresOfClasses
@@ -83,6 +103,17 @@ def trainBOW(trainingPath):
 		'''
 		featuresOfClasses.append((directory, length, numOfWords, totalCounter))
 
+'''
+	takes test path, feature quadruples of classes, and vocabulary size.
+	for all directories in test set, looks at all files in the directory. 
+	takes tokens by tokenize() function.
+	for each class, calculates: log(prob(class))
+	for each token in training set calculates: log(prob(token|class))
+	adds probabilities to find total probability.
+	adds class name - probability pair in a list.
+	selects class with larger probability value.
+	adds true class - (assigned class:how many times) pairs into confusion matrix
+'''
 def test(testPath, featuresOfClasses, vocabSize):
 	global confusionMatrix
 	confusionMatrix = []
@@ -149,7 +180,12 @@ def test(testPath, featuresOfClasses, vocabSize):
 			
 		confusionMatrix.append((directory, trueVSassigned))
 				
-
+'''
+	looks at confusion matrix.
+	for each class counts:  the number of true positive
+							the total number of assignments to this class
+							the true number of documents in this class
+'''
 def evaluate():
 	global truePositiveOfClass
 	global totalAssignedAsClass
@@ -247,6 +283,9 @@ def calAvgF(precision, recall):
 	return 2*precision*recall/(precision+recall)
 
 
+'''
+	from funtional words text creates a list using tokenize() function
+'''
 def initializeFunctionalWords():
 	functWordPath = "./fwords.txt"
 	file = open(functWordPath, encoding=fileEncoding)
@@ -255,6 +294,21 @@ def initializeFunctionalWords():
 	functionalWords = tokenize(file)
 	file.close()
 
+'''
+	takes training path.
+	for all directories in training, looks at all files in the directory. 
+	takes tokens by tokenize() function.
+	for these words creates a counter which counts the unique words in the list.
+	by adding counters, creates a total counter for each directory.
+	** then deletes functional words from the counter. (difference from trainingBOW() function)
+	creates a quadruple for each directory.
+	that quadruple contains: class name
+							 the number of documents in class
+							 the number of words in class
+							 unique word - frequency pairs in class
+	also calculates the total number of files in whole training set and
+					the number of unique words in whole training set.
+'''
 def trainBOWextra(trainingPath):
 	initializeFunctionalWords()
 	global vocabSizeEx
@@ -337,7 +391,7 @@ if __name__ == '__main__':
 
 
 	'''
-		BOW with extra feature
+		BOW without functional words
 	'''
 	trainBOWextra(trainingPath)
 	test(testPath, featuresOfClassesEx, vocabSizeEx)
@@ -359,25 +413,4 @@ if __name__ == '__main__':
 	print("Macro Averaged Precision :\t%s" %macroAvgPrecision)
 	print("Macro Averaged Recall :\t%s" %macroAvgRecall)
 	print("Macro Averaged F value :\t%s" %macroAvgF)
-
-
-'''
-	trainBOW(trainingPath)
-	test(testPath)
-
-	evaluate()
-	calculateRecall()
-	calculatePrecision()
-
-	macroAvgPrecision = calMacroAvgPre()
-	macroAvgRecall = calMacroAvgRec()
-	macroAvgF = calAvgF(macroAvgPrecision, macroAvgRecall)
-
-	microAvgPrecision = calMicroAvgPre()
-	microAvgRecall = calMicroAvgRec()
-	microAvgF = calAvgF(microAvgPrecision, microAvgRecall)
-'''
 	
-
-
-
